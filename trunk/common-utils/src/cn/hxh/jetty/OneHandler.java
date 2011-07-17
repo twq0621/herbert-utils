10bad93fdfaf96278aaf232a18be7a63e9016fc7
herberthuige@gmail.com <herberthuige@gmail.com@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2011-07-17 05:13:15 +0000

    加入jetty

---
+++ trunk/common-utils/src/cn/hxh/jetty/OneHandler.java
@@ -0,0 +1,15 @@
+package cn.hxh.jetty;
+
+import org.eclipse.jetty.server.Server;
+
+public class OneHandler {
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		server.setHandler(new HelloHandler());
+
+		server.start();
+		server.join();
+	}
+
+}
