c94e22f197ad9a76b2c3fd2211e12c912093117b
herberthuige <herberthuige@gmail.com>
2011-07-20 06:10:30 +0000

    添加neo4j和js script engine 支持

---
+++ common-utils/src/cn/hxh/script/GameScriptEngineManager.java
@@ -0,0 +1,48 @@
+package cn.hxh.script;
+
+import java.io.Reader;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class GameScriptEngineManager implements ApplicationContextAware {
+
+	private static final Logger logger = LoggerFactory.getLogger(GameScriptEngineManager.class);
+
+	private ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+	private ScriptEngine scriptEngine = scriptEngineManager.getEngineByExtension("js");
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
+		for (String beanName : beanNames) {
+			scriptEngine.put(beanName, applicationContext.getBean(beanName));
+		}
+	}
+
+	public String execute(Reader reader) {
+		String output = StringUtils.EMPTY;
+		try {
+			scriptEngine.eval("var output = '';");
+			scriptEngine.eval(reader);
+			Object outputObj = scriptEngine.get("output");
+			if (outputObj instanceof String) {
+				output = (String) outputObj;
+			} else if (outputObj instanceof Number) {
+				Number outputNumber = (Number) outputObj;
+				output = outputNumber.toString();
+			}
+		} catch (ScriptException e) {
+			logger.error("", e);
+		}
+		return output;
+	}
+
+}
