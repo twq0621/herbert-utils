23ea878bd21ae2ca0a1669dddcea23419ad9caf1
herberthuige@gmail.com <herberthuige@gmail.com@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2011-09-01 13:33:00 +0000

    添加string测试

--- trunk/common-utils/test/cn/hxh/string/StringTest.java
+++ trunk/common-utils/test/cn/hxh/string/StringTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class StringTest extends TestCase {
 
-	public void testSplitAnd(){
+	public void testSplitAnd() {
 		String xx = "11&22";
 		String[] res = xx.split("&");
 		System.out.println(res.length);
@@ -12,5 +12,17 @@ public class StringTest extends TestCase {
 			System.out.println(string);
 		}
 	}
-	
+
+	public void testStringFormat() {
+		String src = "herbert is %s";
+		src = String.format(src, "No.1");
+		System.out.println(src);
+		src = "herbert is %s, here is %s";
+		src = String.format(src, "No.1", "she");
+		System.out.println(src);
+		src = "herbert is %s, here' is %s";
+		src = String.format(src, "No.1", "she");
+		System.out.println(src);
+	}
+
 }
