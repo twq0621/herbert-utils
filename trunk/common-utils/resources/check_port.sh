7a2f6fdad67f175930b0e14c776625ae0b73620e
herberthuige@gmail.com <herberthuige@gmail.com@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-02-01 10:31:09 +0000

    添加脚本

--- trunk/common-utils/resources/check_port.sh
+++ trunk/common-utils/resources/check_port.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo -e "checking cs"
 perl ./check_port.pl -h 10.40.244.70 -p 9529
-echo -e "checking gs1"
+echo -e "---------------------\nchecking gs1"
 perl ./check_port.pl -h 10.40.244.70 -p 9526
-echo -e "checking gs2"
+echo -e "---------------------\nchecking gs2"
 perl ./check_port.pl -h 10.40.244.70 -p 9526
-echo -e "checking ps"
+echo -e "---------------------\nchecking ps"
 perl ./check_port.pl -h 10.40.244.66 -p 9528
-echo -e "checking ms"
+echo -e "---------------------\nchecking ms"
 perl ./check_port.pl -h 10.40.244.66 -p 9534
-echo -e "checking js"
+echo -e "---------------------\nchecking js"
 perl ./check_port.pl -h 10.40.244.76 -p 9530
