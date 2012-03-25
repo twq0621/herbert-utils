b93d671525507686ef9f7e3165541224fbfd341c
herberthuige@gmail.com <herberthuige@gmail.com@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-03-25 12:31:51 +0000

    改进redis-protobuf框架

--- trunk/SpringRedis/src/cn/hxh/core/ProtobufRedisSerializer.java
+++ trunk/SpringRedis/src/cn/hxh/core/ProtobufRedisSerializer.java
@@ -1,28 +1,23 @@
 package cn.hxh.core;
 
-import java.nio.charset.Charset;
-
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import com.google.protobuf.MessageLite;
 
-public class ProtobufRedisSerializer<T> implements RedisSerializer<T> {
-
-	public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+public class ProtobufRedisSerializer implements RedisSerializer<Object> {
 
 	@Override
-	public byte[] serialize(T t) throws SerializationException {
+	public byte[] serialize(Object t) throws SerializationException {
 		return ((MessageLite) t).toByteArray();
 	}
 
 	/**
 	 * 由于protobuf的本身机制原因，反序列化操作在程序中执行
 	 */
-	@SuppressWarnings("unchecked")
 	@Override
-	public T deserialize(byte[] bytes) throws SerializationException {
-		return (T) bytes;
+	public Object deserialize(byte[] bytes) throws SerializationException {
+		return bytes;
 	}
 
 }
