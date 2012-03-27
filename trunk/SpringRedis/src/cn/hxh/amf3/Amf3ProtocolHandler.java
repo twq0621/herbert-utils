32d1765fed55dc0134de0d3460bd934b671414ac
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-03-27 07:20:23 +0000

    添加as代码生成工具

--- trunk/SpringRedis/src/cn/hxh/amf3/Amf3ProtocolHandler.java
+++ trunk/SpringRedis/src/cn/hxh/amf3/Amf3ProtocolHandler.java
@@ -1,32 +1,48 @@
 package cn.hxh.amf3;
 
+import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
+
 import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import flex.messaging.messages.CommandMessage;
-
 public class Amf3ProtocolHandler extends SimpleChannelHandler {
 
-	public static Logger log = LoggerFactory
+	public static Logger logger = LoggerFactory
 			.getLogger(Amf3ProtocolHandler.class);
 
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
 		if (e.getMessage() != null) {
-//			ChannelManager channelManager = PushServerContext
-//					.getBean(ChannelManager.class);
-//			if (e.getMessage() instanceof CommandMessage) {
-//				channelManager.handleMsg((CommandMessage) e.getMessage(),
-//						e.getChannel());
-//			} else if (e.getMessage() instanceof PushMessage) {
-//				channelManager.handleMsg((PushMessage) e.getMessage(),
-//						e.getChannel());
-//			} else {
-//				log.warn("unkown message {}", e);
-//			}
+			// ChannelManager channelManager = PushServerContext
+			// .getBean(ChannelManager.class);
+			// if (e.getMessage() instanceof CommandMessage) {
+			// channelManager.handleMsg((CommandMessage) e.getMessage(),
+			// e.getChannel());
+			// } else if (e.getMessage() instanceof PushMessage) {
+			// channelManager.handleMsg((PushMessage) e.getMessage(),
+			// e.getChannel());
+			// } else {
+			// log.warn("unkown message {}", e);
+			// }
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+		if (e.getCause() instanceof ClosedChannelException) {
+			logger.info("ClosedChannelException: {}", e);
+		} else if (e.getCause() instanceof IOException) {
+			logger.info("IOException: {}", e.getCause().getMessage());
+		} else {
+			logger.warn("Exception: {}", e.getCause());
+		}
+		if (e.getChannel().isOpen()) {
+			e.getChannel().close();
 		}
 	}
 
---
+++ trunk/SpringRedis/src/cn/hxh/dto/SaoShuC2S.java
@@ -0,0 +1,25 @@
+package cn.hxh.dto;
+
+public class SaoShuC2S {
+
+	private String name;
+
+	private int retCode;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRetCode() {
+		return retCode;
+	}
+
+	public void setRetCode(int retCode) {
+		this.retCode = retCode;
+	}
+
+}
