e767cabece7b1663e4d43032407458d204f1360b
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-03-27 07:20:23 +0000

    添加as代码生成工具

--- SpringRedis/src/cn/hxh/amf3/Amf3ServerPipelineFactory.java
+++ SpringRedis/src/cn/hxh/amf3/Amf3ServerPipelineFactory.java
@@ -9,16 +9,18 @@ import cn.hxh.codec.AMF3Encoder;
 
 public class Amf3ServerPipelineFactory implements ChannelPipelineFactory {
 
+	public static final int MAX_BUFFER_SIZE = 65536;
+
 	@Override
 	public ChannelPipeline getPipeline() throws Exception {
 		ChannelPipeline pipeline = Channels.pipeline();
-		 // 处理coder
-		 pipeline.addLast("decoder", new AMF3Decoder(65536));
-		 pipeline.addLast("encoder", new AMF3Encoder());
-		 //
-		 pipeline.addLast("handler", new Amf3ProtocolHandler());
-		 //
-		 return pipeline;
+		// 处理coder
+		pipeline.addLast("decoder", new AMF3Decoder(MAX_BUFFER_SIZE));
+		pipeline.addLast("encoder", new AMF3Encoder());
+		//
+		pipeline.addLast("handler", new Amf3ProtocolHandler());
+		//
+		return pipeline;
 	}
 
 }
---
+++ SpringRedis/src/cn/hxh/dto/NameDTO.java
@@ -0,0 +1,45 @@
+package cn.hxh.dto;
+
+public class NameDTO {
+
+	private String name;
+
+	private String passwprd;
+
+	private int age;
+
+	private float money;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPasswprd() {
+		return passwprd;
+	}
+
+	public void setPasswprd(String passwprd) {
+		this.passwprd = passwprd;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getMoney() {
+		return money;
+	}
+
+	public void setMoney(float money) {
+		this.money = money;
+	}
+
+}
