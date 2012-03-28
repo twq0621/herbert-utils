910b70711bf9d97191e5295c216952e413be7a4c
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-03-28 04:55:31 +0000

    添加Amf3服务器和客户端

---
+++ trunk/SpringRedis/src/cn/hxh/amf3/Amf3Client.java
@@ -0,0 +1,46 @@
+package cn.hxh.amf3;
+
+import java.net.SocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.ChannelUpstreamHandler;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+public class Amf3Client {
+
+	
+	protected NioClientSocketChannelFactory _factory;
+	
+	private final ClientBootstrap bootstrap;
+
+	public Amf3Client() {
+		_factory = new NioClientSocketChannelFactory(Executors.newCachedThreadPool(),
+				Executors.newCachedThreadPool());
+		bootstrap = new ClientBootstrap(_factory);
+		bootstrap.setOption("tcpNoDelay", true);
+		bootstrap.setOption("connectTimeoutMillis", 10000);
+		bootstrap.setOption("soLinger", 0);//by hxh,断开连接立即释放socket资源
+		bootstrap.setPipelineFactory(pipelineFactory);
+	}
+
+	private final ChannelUpstreamHandlerFactory handlerFactory = new ChannelUpstreamHandlerFactory() {
+		public ChannelUpstreamHandler getChannelUpstreamHandler() {
+			return new Amf3ClientChannelHandler();
+		}
+	};
+
+	private final ChannelPipelineFactory pipelineFactory = new Amf3PipelineFactory(
+			handlerFactory);
+
+	public NettyRpcChannel blockingConnect(SocketAddress sa) {
+		return new NettyRpcChannel(bootstrap.connect(sa).awaitUninterruptibly()
+				.getChannel());
+	}
+
+	public void shutdown() {
+		bootstrap.releaseExternalResources();
+	}
+
+}
--- trunk/SpringRedis/src/cn/hxh/amf3/Amf3PipelineFactory.java
+++ trunk/SpringRedis/src/cn/hxh/amf3/Amf3PipelineFactory.java
@@ -7,18 +7,25 @@ import org.jboss.netty.channel.Channels;
 import cn.hxh.codec.AMF3Decoder;
 import cn.hxh.codec.AMF3Encoder;
 
-public class Amf3ServerPipelineFactory implements ChannelPipelineFactory {
+public class Amf3PipelineFactory implements ChannelPipelineFactory {
 
-	public static final int MAX_BUFFER_SIZE = 65536;
+	public static final int MAX_BUFFER_SIZE = 1048576;
+
+	private final ChannelUpstreamHandlerFactory handlerFactory;
+
+	public Amf3PipelineFactory(ChannelUpstreamHandlerFactory handlerFactory) {
+		this.handlerFactory = handlerFactory;
+	}
 
 	@Override
 	public ChannelPipeline getPipeline() throws Exception {
 		ChannelPipeline pipeline = Channels.pipeline();
 		// 处理coder
 		pipeline.addLast("decoder", new AMF3Decoder(MAX_BUFFER_SIZE));
+//		pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));
 		pipeline.addLast("encoder", new AMF3Encoder());
 		//
-		pipeline.addLast("handler", new Amf3ProtocolHandler());
+		pipeline.addLast("handler", handlerFactory.getChannelUpstreamHandler());
 		//
 		return pipeline;
 	}
