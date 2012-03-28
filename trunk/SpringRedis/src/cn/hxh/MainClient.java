4d89ffca3edbb451da25a5517ad93d53a4681d65
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-03-28 13:05:30 +0000

    code improve

--- trunk/SpringRedis/src/cn/hxh/MainClient.java
+++ trunk/SpringRedis/src/cn/hxh/MainClient.java
@@ -1,5 +1,7 @@
 package cn.hxh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,15 +12,18 @@ import cn.hxh.sample.SampleClient;
 
 public class MainClient {
 
+	private static Logger logger = LoggerFactory.getLogger(MainClient.class);
+
 	public static void main(String[] args) {
 		ApplicationContext factory = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
+		logger.info("spring init success!,factory={}", factory);
 		Amf3Client amf3Client = new Amf3Client();
 		SampleClient sampleClient = new SampleClient(amf3Client);
 		sampleClient.connect("192.168.83.94", 8653);
-		 GetNewRole_C2S msg = new GetNewRole_C2S();
-		 msg.setQueryDay("2012-03-14");
-		 sampleClient.send(msg);
+		GetNewRole_C2S msg = new GetNewRole_C2S();
+		msg.setQueryDay("2012-03-14");
+		sampleClient.send(msg);
 		GetOnlineNames_C2S msg2 = new GetOnlineNames_C2S();
 		sampleClient.send(msg2);
 	}
