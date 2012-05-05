1e34bf72ac0f827df998ad2dd7e0d261be35526d
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-05-05 04:46:06 +0000

    脚本更新

---
+++ common-utils/resources/shell/run2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SERVER_SID=$1
+cd /shiyue/svn_server
+echo "cleaning svn directory...."
+svn revert --depth=infinity .
+echo "fetching latest source code...."
+svn up
+cd game_app
+ant build
+if [ $? -ne  0 ];
+then
+        echo -e "\e[1;31m build failed! \e[0m"
+        exit 1;
+fi
+cd ../gameApp/
+sed -i 's/\(server\.sid=\)[0-9]\+/\18/' $PWD/WEB-INF/gameservices/serverparam.properties
+tar -zcvf WEB-INF.tar.gz WEB-INF/
+cp WEB-INF.tar.gz /shiyue/app/server/baishe
+echo "stopping resin server......"
+sh /opt/resin-3.1.12/bin/httpd.sh stop
+cd /shiyue/app/server/baishe
+rm -rf WEB-INF/
+tar -zxvf WEB-INF.tar.gz
+echo "starting resin server...."
+sh /opt/resin-3.1.12/bin/httpd.sh start
