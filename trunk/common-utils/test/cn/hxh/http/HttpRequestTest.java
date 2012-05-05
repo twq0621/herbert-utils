38e52b83dddc97de695eead4f7d6370580758700
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-05-05 04:46:06 +0000

    脚本更新

--- trunk/common-utils/test/cn/hxh/http/HttpRequestTest.java
+++ trunk/common-utils/test/cn/hxh/http/HttpRequestTest.java
@@ -5,10 +5,13 @@ import junit.framework.TestCase;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import cn.hxh.HttpRequestUtil;
 
 public class HttpRequestTest extends TestCase {
 
@@ -22,6 +25,74 @@ public class HttpRequestTest extends TestCase {
 			HttpGet httpGet = new HttpGet(url);
 			ResponseHandler<String> responsehandler = new BasicResponseHandler();
 			ret = httpClient.execute(httpGet, responsehandler);
+			logger.info("http response={}", ret);
+		} catch (Exception e) {
+			logger.error("error", e);
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+	
+	public void testSendJson() {
+		String ret = "";
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			String url = "http://192.168.1.250:8080/gameApp/cs_json_api";
+			byte[] respByte = HttpRequestUtil.sendRequestV2(url,
+					"{\"head\":{\"cmd\":\"stateOnline\",\"para\":\"userNums\",\"length\":\"\",\" timestamp \":\"\",\"len\":\"\"},\"body\":{\"sid\":[1,2], \"sig\":\"加密验证\" }}",
+					null, "POST", "application/json");
+			ret = new String(respByte, "UTF-8");
+			logger.info("http response={}", ret);
+		} catch (Exception e) {
+			logger.error("error", e);
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+	
+	public void testPlayerQuery() {
+		String ret = "";
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			String url = "http://192.168.1.250:8080/gameApp/cs_json_api";
+			byte[] respByte = HttpRequestUtil.sendRequestV2(url,
+					"{\"head\":{\"cmd\":\"playerQueries\",\"para\":\"player\",\"length\":\"\",\" timestamp \":\"\",\"len\":\"\"},\"body\":{\"area\":\"\",\"sig\":\"加密验证\" ,\"player\":\"角色名\",\"openid\":\"外部id\",\"innerid\":\"内部id\", \"playerid\":\"14631\"}}",
+					null, "POST", "application/json");
+			ret = new String(respByte, "UTF-8");
+			logger.info("http response={}", ret);
+		} catch (Exception e) {
+			logger.error("error", e);
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+	
+	public void testPropsQueries() {
+		String ret = "";
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			String url = "http://192.168.1.250:8080/gameApp/cs_json_api";
+			byte[] respByte = HttpRequestUtil.sendRequestV2(url,
+					"{\"head\":{\"cmd\":\"propsQueries\",\"para\":\"props\",\"length\":\"\",\" timestamp \":\"\",\"len\":\"\"},\"body\":{\"area\":\"\",\"sig\":\"加密验证\" ,\"player\":\"角色名\",\"openid\":\"外部id\",\"innerid\":\"内部id\", \"playerid\":\"14631\"}}",
+					null, "POST", "application/json");
+			ret = new String(respByte, "UTF-8");
+			logger.info("http response={}", ret);
+		} catch (Exception e) {
+			logger.error("error", e);
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+
+	public void testTasksQueries() {
+		String ret = "";
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			String url = "http://192.168.1.250:8080/gameApp/cs_json_api";
+			byte[] respByte = HttpRequestUtil.sendRequestV2(url,
+					"{\"head\":{\"cmd\":\"tasksQueries\",\"para\":\"tasks\",\"length\":\"\",\" timestamp \":\"\",\"len\":\"\"},\"body\":{\"area\":\"\",\"sig\":\"加密验证\" ,\"player\":\"角色名\",\"openid\":\"外部id\",\"innerid\":\"内部id\", \"playerid\":\"14631\"}}",
+					null, "POST", "application/json");
+			ret = new String(respByte, "UTF-8");
 			logger.info("http response={}", ret);
 		} catch (Exception e) {
 			logger.error("error", e);
