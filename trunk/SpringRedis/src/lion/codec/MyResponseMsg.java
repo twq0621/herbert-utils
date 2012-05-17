a63d47950cfe5a0dbaf00ee8cc8ba34fc87e78d9
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-05-17 14:48:54 +0000

    添加多线程处理

---
+++ trunk/SpringRedis/src/lion/codec/MyResponseMsg.java
@@ -0,0 +1,23 @@
+package lion.codec;
+
+/**
+ * 此接口声明了响应消息编码函数，服务端生产的每个响应消息都必须实现此接口。<br>
+ * 这里声明的函数在protocolEncoder中会被自动调用，编码成byte stream发送到客户端。
+ * 
+ * @author serv_dev
+ * 
+ */
+public interface MyResponseMsg{
+	/**
+	 * 获得消息号 
+	 * @return
+	 */
+	public int getMsgCode();
+	
+	public int getTotalBytes();
+	/**
+	 * 设置消息号
+	 * @param code
+	 */
+	public void setMsgCode(int code); 
+}
---
+++ trunk/SpringRedis/src/lion/core/MyExecuterPool.java
@@ -0,0 +1,65 @@
+package lion.core;
+
+import lion.codec.IServerDispatcher;
+import lion.codec.IThreadProcessor;
+import lion.codec.MyRequestMsg;
+
+import org.jboss.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyExecuterPool {
+
+	public static final Logger logger = LoggerFactory.getLogger(MyExecuterPool.class);
+
+	private IServerDispatcher gameServer;
+	
+	public MyExecuterPool(IServerDispatcher gameServer) {
+		this.gameServer = gameServer;
+	}
+	
+	public void executeIoRequest(final Channel channel, final Object remoteObj) {
+		MyRequestMsg request = (MyRequestMsg) remoteObj;
+		GamePlayer player = GamePlayer.getGamePlayer(channel);
+		if (player == null) {
+			logger.info("player is null");
+			return;
+		}
+		player.setLastTime(System.currentTimeMillis());
+		int msgCode = request.getMsgCode();
+		// 该消息处理是系统消息，但ip不是授权ip
+//		if (msgCode < 10000 && !gameServer.checkIP(player.getAddress())) {
+//			logger.error("invalidate request from ?", player.getAddress());
+//			return;
+//		}
+		MsgProcessor processor = gameServer.getMsgProcessor(msgCode);
+		if (processor == null) {
+			return;
+		}
+		if (!processor.isEnable()) {
+			logger.warn("msg processor is closed " + msgCode);
+			return;
+		}
+		if (!(processor instanceof INotNeedAuthProcessor) && !player.isValidate()) {
+			return;
+		}
+		// GameServer.concurrentMsgProcessorcount.incrementAndGet();
+		// 该消息处理不需要会话通过验证，或者会话已经通过验证
+		if (player.getSessionType() == GamePlayer.Session_Type_Chat) {
+			try {
+				processor.process(player, request);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		} else if (processor instanceof IThreadProcessor) {
+			try {
+				processor.process(player, request);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		} else {
+			player.addRequest(request);
+		}
+	}
+
+}
