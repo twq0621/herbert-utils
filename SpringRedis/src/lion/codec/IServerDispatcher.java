3111f88b8ba29cf6916ca38e7c092e24a39c1094
herberthuige <herberthuige@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2012-05-17 14:48:54 +0000

    添加多线程处理

---
+++ SpringRedis/src/lion/codec/IServerDispatcher.java
@@ -0,0 +1,50 @@
+package lion.codec;
+
+import net.snake.netio.message.MsgDispatcher;
+import net.snake.netio.player.GamePlayer;
+
+/**
+ * @author serv_devr
+ */
+public interface IServerDispatcher {
+	/**
+	 * 消息分发器
+	 * 
+	 * @return
+	 */
+	public MsgDispatcher getMsgDispatcher();
+
+	/**
+	 * 工作路径
+	 * 
+	 * @return
+	 */
+	public String getWorkPath();
+
+	/**
+	 * 消息处理器配置文件地址
+	 * 
+	 * @return
+	 */
+	public String getProccessProp();
+
+	/**
+	 * 检查IP是否是管理IP
+	 * @param ip
+	 * @return true是
+	 */
+	public boolean checkIP(String ip);
+
+	/**
+	 * 返回消息号处理类
+	 * @param msgCode
+	 * @return
+	 */
+	public MsgProcessor getMsgProcessor(int msgCode);
+
+	/**
+	 * session关闭处理
+	 * @param player
+	 */
+	public void sessionClosed(GamePlayer player);
+}
