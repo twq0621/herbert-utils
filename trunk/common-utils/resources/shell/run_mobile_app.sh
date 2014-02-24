3abc7d0f5a18e86ec73e1cbc93dd28ebe953a9b0
herberthuige@gmail.com <herberthuige@gmail.com@aa37def6-18b5-fd36-5fc6-d360b81b0eb2>
2014-02-24 08:24:07 +0000

    update

--- trunk/common-utils/resources/shell/run_mobile_app.sh
+++ trunk/common-utils/resources/shell/run_mobile_app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-cd /opt/server_source/ServerSide/mobile_app
+PROCESS="mobile_server"
+cd /opt/server_source/ServerSide
 echo "cleaning svn directory...."
 svn revert --depth=infinity .
 echo "fetching latest source code...."
 svn up
+cd mobile_app
 mvn -Dmaven.package.phase=true -Dmaven.antrun.skip=true clean package
 if [ $? -ne  0 ];
 then
@@ -15,13 +17,9 @@ cd /opt/deploy/
 rm -rf mobile_server-0.2.1
 unzip mobile_server-0.2.1-bin.zip
 echo "stop the mobile server"
+PID=`ps -ef|grep "${PROCESS}" | grep -v "grep"|awk '{print $2} '`
 sh mobile_server-0.2.1/stop.sh
-while [ $? -eq  0 ]
-do
-    echo "checking server stop...."
-    sleep 3
-    nc -vv -z -w 2 192.168.78.31 8653 2> /dev/null
-done
-echo "starting mobile server......"
+while [ -e /proc/${PID} ]; do sleep 0.1; done
+echo "starting ${PROCESS}......"
 cd mobile_server-0.2.1
 sh start.sh
